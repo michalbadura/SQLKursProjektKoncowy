22cdd586f029122154ef41345a592bef2f83396d
Michal Badura <mbadura@interia.eu>
2022-03-30 19:57:48 +0200

    zadanie 3, polowa

--- 003_AnalizaKont.sql
+++ 003_AnalizaKont.sql
@@ -7,3 +7,12 @@ kwota udzielonych pożyczek (malejąco),
 
 Pod uwagę bierzemy tylko spłacone pożyczki.
  */
+
+
+SELECT account_id,
+       COUNT(financial3_7.loan.loan_id) AS "liczba udzielonych pozyczek",
+       SUM(financial3_7.loan.amount)AS "kwota udzielonych pozyczek",
+       AVG(financial3_7.loan.amount)AS "srednia kwota pozyczki"
+FROM financial3_7.loan
+WHERE loan.status IN ('A', 'C')                -- Status pozyczki splacony A i C, niesplacony to B i D
+GROUP BY account_id;
