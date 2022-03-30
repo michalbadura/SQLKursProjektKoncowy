ca1f1ecd24415e518ccfc910c136e488423c4269
Michal Badura <mbadura@interia.eu>
2022-03-30 20:23:40 +0200

    zadanie 3, skonczone

--- 003_AnalizaKont.sql
+++ 003_AnalizaKont.sql
@@ -9,10 +9,21 @@ Pod uwagę bierzemy tylko spłacone pożyczki.
  */
 
 
-SELECT account_id,
-       COUNT(financial3_7.loan.loan_id) AS "liczba udzielonych pozyczek",
-       SUM(financial3_7.loan.amount)AS "kwota udzielonych pozyczek",
-       AVG(financial3_7.loan.amount)AS "srednia kwota pozyczki"
+
+
+
+
+WITH analiza_kont AS (
+    SELECT
+          account_id,
+          SUM(financial3_7.loan.amount) AS "kwota udzielonych pozyczek",
+          COUNT(financial3_7.loan.amount) AS "liczba udzielonych pozyczek",
+          AVG(financial3_7.loan.amount) AS "srednia kwota pozyczki"
 FROM financial3_7.loan
-WHERE loan.status IN ('A', 'C')                -- Status pozyczki splacony A i C, niesplacony to B i D
-GROUP BY account_id;
+    WHERE loan.status IN ('A', 'C') -- Status pozyczki splacony A i C, niesplacony to B i D
+    GROUP BY account_id
+)
+SELECT *,
+       ROW_NUMBER() OVER (ORDER BY `kwota udzielonych pozyczek` DESC) AS "ranking kwota udzielonych pozyczek",
+       ROW_NUMBER() OVER (ORDER BY `liczba udzielonych pozyczek` DESC) AS "ranking liczba udzielonych pozyczek"
+FROM analiza_kont;
