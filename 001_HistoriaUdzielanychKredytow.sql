4c85a79fb267324ed205034174d86a831740893c
Michal Badura <mbadura@interia.eu>
2022-03-31 22:18:13 +0200

    update

--- 001_HistoriaUdzielanychKredytow.sql
+++ 001_HistoriaUdzielanychKredytow.sql
@@ -12,3 +12,15 @@ sumaryczna kwota pożyczek,
 średnia kwota pożyczki,
 całkowita liczba udzielonych pożyczek.
  */
+
+
+ SELECT
+    extract(YEAR FROM date) as loan_year,
+    extract(QUARTER FROM date) as loan_quarter,
+    extract(MONTH FROM date) as loan_month,
+    sum(payments) as loans_total,
+    avg(payments) as loans_avg,
+    count(payments) as loans_count
+FROM financial3_7.loan
+GROUP BY 1, 2, 3 WITH ROLLUP
+ORDER BY 1, 2, 3
